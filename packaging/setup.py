c9896e2b3f4065d6a941a6b742f3edb02cbba461
Kyle Lahnakoski <kyle@lahnakoski.com>
2024-05-04 13:50:32 -0400

    update version number

--- packaging/setup.py
+++ packaging/setup.py
@@ -15,6 +15,6 @@ setup(
     name='mo-streams',
     packages=["mo_streams"],
     url='https://github.com/klahnakoski/mo-streams',
-    version='1.619.24125',
+    version='1.620.24125',
     zip_safe=False
 )
